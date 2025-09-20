baada1b7dabe53395fff35991874d0efc28e7aff
Andrii <andriishtaher@gmail.com>
2025-09-20 22:49:28 +0200

    fix: solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -4,60 +4,65 @@
  * @param {Object} state
  * @param {Object[]} actions
  *
- * @return {Object[]}
+ * @return {string}
  */
 function transformStateWithClones(state, actions) {
-  const actionsHistory = [{ ...state }];
+  const stateCopy = { ...state };
+  const actionsHistory = [{ ...stateCopy }];
   let status = false;
 
   for (const action of actions) {
     let changes = { ...actionsHistory.at(-1) };
+    let wasChanged = false;
 
-    if (action.type === 'addProperties') {
-      let wasChanged = false;
+    switch (action.type) {
+      case 'addProperties':
+        for (const act in action.extraData) {
+          if (actionsHistory.length === 1 && !status) {
+            actionsHistory[0][act] = action.extraData[act];
+            wasChanged = true;
+          } else {
+            changes[act] = action.extraData[act];
+          }
+        }
 
-      for (const act in action.extraData) {
+        if (wasChanged) {
+          status = true;
+        }
+
+        if (actionsHistory.length > 1) {
+          actionsHistory.push({ ...changes });
+          changes = {};
+        }
+        continue;
+
+      case 'clear':
         if (actionsHistory.length === 1 && !status) {
-          actionsHistory[0][act] = action.extraData[act];
-          wasChanged = true;
+          actionsHistory[0] = {};
+          status = true;
         } else {
-          changes[act] = action.extraData[act];
+          actionsHistory.push({});
         }
-      }
+        continue;
 
-      if (wasChanged) {
-        status = true;
-      }
-
-      if (actionsHistory.length > 1) {
-        actionsHistory.push({ ...changes });
-        changes = {};
-      }
-    }
-
-    if (action.type === 'clear') {
-      if (actionsHistory.length === 1 && !status) {
-        actionsHistory[0] = {};
-        status = true;
-      } else {
-        actionsHistory.push({});
-      }
-    }
-
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        if (key in changes) {
-          delete changes[key];
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          if (key in changes) {
+            delete changes[key];
+          }
         }
-      }
 
-      if (actionsHistory.length === 1 && !status) {
-        actionsHistory[0] = { ...changes };
-        status = true;
-      } else {
-        actionsHistory.push({ ...changes });
-        changes = {};
-      }
+        if (actionsHistory.length === 1 && !status) {
+          actionsHistory[0] = { ...changes };
+          status = true;
+        } else {
+          actionsHistory.push({ ...changes });
+          changes = {};
+        }
+        continue;
+
+      default:
+        return 'Unknown action type: ' + action.type;
     }
   }
 
